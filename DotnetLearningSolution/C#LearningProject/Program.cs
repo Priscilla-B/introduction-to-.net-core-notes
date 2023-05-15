d81fdd99b94f5f3697323e797e5b30bfc6d4fbe2
Priscilla Baah <pyfbaah@gmail.com>
2023-05-15 08:38:37 +0000

    add notes on the structure of a C# program

--- DotnetLearningSolution/C#LearningProject/Program.cs
+++ DotnetLearningSolution/C#LearningProject/Program.cs
@@ -1,26 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+// an example of an inbuilt namespace
 
 namespace CSLearningProject
+    // namespaces are used to group classes to keep them organized
 {
     class Program
     {
         static void Main(string[] args)
+            // main method serves as the entry point for the program
+            // where the program starts basically.
+            // needs to be "static", which means it doesn't change dynamically
+            // as the program runs
+            // "void" means the method does not return anything
+            // string[] args represents a string array of arguments
         {
-            // Branches priscilla = new Branches();
-            // priscilla.BranchText();
-            Console.WriteLine("Suprise mother....");
-
-            Console.WriteLine("Enter first number");
-            int firstNum = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"The first number is {firstNum}");
-
-            Console.WriteLine("Enter second number");
-            int secondNum = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"The second number is {secondNum}");
-        
-            Console.WriteLine($"The sum of your numbers is  {firstNum + secondNum}");
-            Console.ReadLine();
+            Branches branch = new Branches();
+            branch.BranchText();
+         
 
         }
     }
