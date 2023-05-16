24c87c6916bedb15a6a2c6633ab82fc10459e1e1
Priscilla Baah <pyfbaah@gmail.com>
2023-05-16 15:59:26 +0000

    make deposits and withdrawals

--- DotnetLearningSolution/MySuperBank-OOP-Project/Program.cs
+++ DotnetLearningSolution/MySuperBank-OOP-Project/Program.cs
@@ -6,8 +6,27 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Priscilla");
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+            var account = new BankAccount("Priscilla", 10000);
+            Console.WriteLine(
+                $"Account {account.Number} was created for {account.Owner} with {account.Balance}"
+                );
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine($"Account balance is {account.Balance}");
+
+            // account.MakeDeposit(-300, DateTime.Now, "Negative deposit");
+
+            // test that initial balance is positive
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -499);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("continue");
         }
     }
 }
